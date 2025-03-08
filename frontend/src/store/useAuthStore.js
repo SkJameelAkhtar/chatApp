b4e953cc003c16e5fe8ef50b5e396fd36188cc1c
sja <skjameelakhtar2003@gmail.com>
2025-03-08 21:08:16 +0530

    initial commit

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isUpdatingProfile: false,
+
+  isCheckingAuth: true,
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+
+      set({ authUser: res.data });
+      get().connectSocket();
+    } catch (error) {
+      console.log("Error in checkAuth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signUp: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      get().connectSocket();
+    } catch (error) {
+      console.log("Error in signUp:", error);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+}));
